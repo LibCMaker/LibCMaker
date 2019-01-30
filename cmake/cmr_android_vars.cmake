27e4e7a62b2c579e5fc37ae57e0716df426bd6f9
NikitaFeodonit <nfeodonit@yandex.com>
2019-01-30 14:04:45 +0300

    Add license header.

--- cmake/cmr_android_vars.cmake
+++ cmake/cmr_android_vars.cmake
@@ -1,3 +1,26 @@
+# ****************************************************************************
+#  Project:  LibCMaker
+#  Purpose:  A CMake build scripts for build libraries with CMake
+#  Author:   NikitaFeodonit, nfeodonit@yandex.com
+# ****************************************************************************
+#    Copyright (c) 2017-2018 NikitaFeodonit
+#
+#    This file is part of the LibCMaker project.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published
+#    by the Free Software Foundation, either version 3 of the License,
+#    or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#    See the GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+# ****************************************************************************
+
 # Some variables are defined in <NDK>/build/cmake/android.toolchain.cmake
 # and may be used after it. See 'android.toolchain.cmake'.
 #  ANDROID_NDK_REVISION
