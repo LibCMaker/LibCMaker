e99f45fa01c1f2249929b8f037488a2b80f006b0
NikitaFeodonit <nfeodonit@yandex.com>
2019-08-15 06:33:22 +0300

    cmr_USE_STATIC_RUNTIME for MSVC, GNU and Clang

--- cmake/cmr_build_rules.cmake
+++ cmake/cmr_build_rules.cmake
@@ -77,32 +77,59 @@ function(cmr_build_rules)
   endif()
 
   # Set compile flags.
-  if(cmr_USE_MSVC_STATIC_RUNTIME AND MSVC AND NOT BUILD_SHARED_LIBS)
-    # Set MSVC static runtime flags for all configurations.
+  if(MSVC)
+    # Determine MSVC runtime library flag
+    set(MSVC_LIB_USE "/MD")
+    set(MSVC_LIB_REPLACE "/MT")
+    if(cmr_USE_STATIC_RUNTIME AND NOT BUILD_SHARED_LIBS)
+      set(MSVC_LIB_USE "/MT")
+      set(MSVC_LIB_REPLACE "/MD")
+    endif()
+    # Set MSVC runtime flags for all configurations
     # See:
     # https://stackoverflow.com/a/20804336
     # https://stackoverflow.com/a/14172871
     foreach(cfg "" ${CMAKE_CONFIGURATION_TYPES})
-      set(c_flag_var   CMAKE_C_FLAGS)
+      set(c_flag_var CMAKE_C_FLAGS)
       set(cxx_flag_var CMAKE_CXX_FLAGS)
       if(cfg)
         string(TOUPPER ${cfg} cfg_upper)
         set(c_flag_var   "${c_flag_var}_${cfg_upper}")
         set(cxx_flag_var "${cxx_flag_var}_${cfg_upper}")
       endif()
-      if(${c_flag_var} MATCHES "/MD")
-        string(REPLACE "/MD" "/MT" ${c_flag_var} "${${c_flag_var}}")
+      if(${c_flag_var} MATCHES ${MSVC_LIB_REPLACE})
+        string(REPLACE
+          ${MSVC_LIB_REPLACE} ${MSVC_LIB_USE} ${c_flag_var} "${${c_flag_var}}"
+        )
         set(${c_flag_var} ${${c_flag_var}} CACHE STRING
           "Flags used by the C compiler during ${cfg_upper} builds." FORCE
         )
       endif()
-      if(${cxx_flag_var} MATCHES "/MD")
-        string(REPLACE "/MD" "/MT" ${cxx_flag_var} "${${cxx_flag_var}}")
+      if(${cxx_flag_var} MATCHES ${MSVC_LIB_REPLACE})
+        string(REPLACE
+          ${MSVC_LIB_REPLACE} ${MSVC_LIB_USE} ${cxx_flag_var} "${${cxx_flag_var}}"
+        )
         set(${cxx_flag_var} ${${cxx_flag_var}} CACHE STRING
           "Flags used by the CXX compiler during ${cfg_upper} builds." FORCE
         )
       endif()
     endforeach()
+
+  elseif(("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+      OR ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang"))
+    if(cmr_USE_STATIC_RUNTIME AND NOT BUILD_SHARED_LIBS)
+      if(MINGW)
+        set(STATIC_LINKER_FLAGS "-static")
+      else()
+        set(STATIC_LINKER_FLAGS "-static-libgcc -static-libstdc++")
+      endif()
+      set(CMAKE_EXE_LINKER_FLAGS
+        "${CMAKE_EXE_LINKER_FLAGS} ${STATIC_LINKER_FLAGS}"
+      )
+      set(CMAKE_SHARED_LINKER_FLAGS
+        "${CMAKE_SHARED_LINKER_FLAGS} ${STATIC_LINKER_FLAGS}"
+      )
+    endif()
   endif()
 
   # Configure, build and install rules.
