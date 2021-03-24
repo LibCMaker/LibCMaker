cbed141033cfe08f9024f596218c6c4146d41426
NikitaFeodonit <nfeodonit@yandex.com>
2021-03-24 11:10:32 +0300

    Add cmr_print_system_info()

--- cmake/cmr_print_system_info.cmake
+++ cmake/cmr_print_system_info.cmake
@@ -3,7 +3,7 @@
 #  Purpose:  A CMake build scripts for build libraries with CMake
 #  Author:   NikitaFeodonit, nfeodonit@yandex.com
 # ****************************************************************************
-#    Copyright (c) 2017-2018 NikitaFeodonit
+#    Copyright (c) 2017-2021 NikitaFeodonit
 #
 #    This file is part of the LibCMaker project.
 #
@@ -21,8 +21,37 @@
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 # ****************************************************************************
 
-include(cmr_print_debug)
-include(cmr_print_error)
 include(cmr_print_status)
-include(cmr_print_system_info)
-include(cmr_print_value)
+
+function(cmr_print_system_info)
+  if(UNIX AND NOT APPLE AND NOT ANDROID)
+    set(system_NAME "Linux")
+  elseif(APPLE AND NOT IOS)
+    set(system_NAME "macOS")
+  elseif(WIN32)
+    set(system_NAME "Windows")
+  elseif(ANDROID)
+    set(system_NAME "Android")
+  elseif(IOS)
+    set(system_NAME "iOS")
+  endif()
+
+  if(MINGW)
+    set(system_NAME "${system_NAME}_MinGW")
+  endif()
+
+  set(compiler_NAME "${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION}")
+  if(MSVC)
+    set(compiler_NAME
+      "${compiler_NAME} ${CMAKE_GENERATOR_PLATFORM} ${CMAKE_GENERATOR_TOOLSET}"
+    )
+  endif()
+
+  cmr_print_status("============================================================")
+  cmr_print_status("Host system:  ${CMAKE_HOST_SYSTEM}")
+  cmr_print_status("System:       ${system_NAME}, ${CMAKE_SYSTEM}")
+  cmr_print_status("Compiler:     ${compiler_NAME}")
+  cmr_print_status("CMake:        ${CMAKE_VERSION}")
+  cmr_print_status("Build type:   ${CMAKE_BUILD_TYPE}")
+  cmr_print_status("============================================================")
+endfunction()
